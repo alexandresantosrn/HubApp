e833a8fa8c7662bb9ff8515d15df4ec8f4000c7a
Alexandre Dantas dos Santos <allexsantosrn@gmail.com>
2025-09-24 21:37:42 -0300

    Criando HubApp.

---
+++ HubApp/app/src/main/java/com/example/hubapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hubapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.card.MaterialCardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val cardCalculator = findViewById<MaterialCardView>(R.id.cardCalculator)
+        val cardBasketball = findViewById<MaterialCardView>(R.id.cardBasketball)
+        val cardConverter = findViewById<MaterialCardView>(R.id.cardConverter)
+    }
+}
